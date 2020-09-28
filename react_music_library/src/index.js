dc3c87d1b3fed0fb085579b77b295f2278868b42
cwadeking <charles.w.king.cwk@gmail.com>
2020-09-27 21:11:50 -0500

    trying to go back to square one, get things working, and then make it better.  Trying to create state for a class component with the axios call, but am not getting very far.

--- react_music_library/src/index.js
+++ react_music_library/src/index.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Request from 'axios-react';
-import Table from '/Users/charl/Desktop/TerbiumNightClass/ReactMusicLibrary/react_music_library/src/table';
+import axios from 'axios'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+//import Row from './row.js'
+//import './table.js';
 
-const Music = () => (
-  <Request
-  config ={{
-    method:'get',
-    url:'www.devcodecampmusiclibrary.com/api/music',
-  }}
-  >
-    {({loading, response, error, refetch, networkStatus}) => (
-      <Table/>
-    )}
-  </Request>
-)
+class App extends Component (){   
+     state = {
+         data: null,      
+
+    }
+     
+    componentDidMount(){
+        let musicLibrary = this.getDataSetFromApi();
+        this.setState({
+            data: musicLibrary,
+        });
+    }
+       
+    getDataSetFromApi(){
+        let responseString;
+        axios.get(`http://www.devcodecampmusiclibrary.com/api/music`)       
+        return responseString;
+    }
+    render(){
+       
+
+            return(
+                <table>
+                    <tr>
+                        <p>{this.state.data}</p>
+                    </tr>                
+                </table>
+            
+            );
+        
+
+    }
+}
+
+
+
+export default App;
 
 ReactDOM.render(
-  <Table />,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    document.getElementById('root')
+    );
+                // constructor(props){
+                //     super();
+                //     this.state = {
+                //         table: makeAxiosCall(),
+                //     };
+                // }
--- react_music_library/src/row.js
+++ react_music_library/src/row.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function Row(props){
+    
     return(
-        <tr className ="tr">
-            <td>
-                {props.value}
-            </td>
-        </tr>
+        <span>
+            <td>{props.song}</td>
+        </span>
     );    
 }
 
 export default Row;
+{/* <td>{props.album}</td>
+<td>{props.artist}</td>
+<td>{props.genre}</td>
+<td>{props.releaseDate}</td> */}
